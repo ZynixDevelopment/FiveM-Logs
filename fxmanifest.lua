f5caefe4db323dee9dbfd4981f6e9cd7bca4a28d
Zynix <162629879+ZynixDevelopment@users.noreply.github.com>
2025-05-19 10:09:07 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'ZynixDevelopment'
+description 'Script complet de logs pour FiveM avec envoi sur Discord'
+version '1.0.0'
+
+server_script 'server.lua'
